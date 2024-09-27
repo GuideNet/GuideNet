8b04fe65138a1121951b91d1274ce99ef9f3f153
Lily Yang <lily.yang1@uwaterloo.ca>
2024-09-27 17:30:55 -0400

    vercel deployment set up

--- index.js
+++ index.js
@@ -120,6 +120,14 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage })
 
+// Serve static files in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
+
 // Start the server
 const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
