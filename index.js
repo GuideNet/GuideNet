10c5fd30a47b459ffddace747c2ed51c7c2eeb17
Lily Yang <lily.yang1@uwaterloo.ca>
2024-10-08 17:49:48 -0400

    debug

--- index.js
+++ index.js
@@ -51,10 +51,7 @@ app.use("/api/chats", chatRoutes)
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err))
 
@@ -147,7 +144,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Start the server
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
