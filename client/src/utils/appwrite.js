c8d2e1e5ad1ecd9a993a07115145baa1b0c234cb
Lily Yang <lily.yang1@uwaterloo.ca>
2024-10-11 17:18:58 -0400

    update appwrite configuration

--- client/src/utils/appwrite.js
+++ client/src/utils/appwrite.js
@@ -1,8 +1,8 @@
 import { Client, Account } from "appwrite"
 
 const client = new Client()
-  .setEndpoint(process.env.APPWRITE_ENDPOINT)
-  .setProject(process.env.APPWRITE_PROJECT_ID)
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject("6706e79b0017b779b62c")
 
 const account = new Account(client)
 
